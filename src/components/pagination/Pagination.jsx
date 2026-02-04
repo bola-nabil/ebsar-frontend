80e82c3856cc8b6befeb1c4f82263d5d6b02e5b5
Bola Nabil <bolanabil404@gmail.com>
2026-02-04 14:08:46 +0200

    add pagination

---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Pagination = ({ totalPages, currentPage, paginate }) => {
+  return (
+    <>
+      {totalPages > 1 && (
+        <nav>
+          <ul className="pagination justify-content-center mt-5">
+            {[...Array(totalPages)].map((_, i) => (
+              <li
+                key={i}
+                className={`page-item ${currentPage === i + 1 ? "active" : ""}`}
+              >
+                <button onClick={() => paginate(i + 1)} className="page-link">
+                  {i + 1}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
+    </>
+  );
+};
+
+export default Pagination;
