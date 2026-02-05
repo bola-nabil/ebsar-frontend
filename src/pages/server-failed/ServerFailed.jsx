a1ff99712971f051d5c8dc4943a075b6a40267b7
Bola Nabil <bolanabil404@gmail.com>
2026-02-05 13:12:31 +0200

    make dir to contain code and style

---
+++ src/pages/server-failed/ServerFailed.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PageTitle from "../../components/ui/PageTitle";
+import "./server-failed.css";
+
+const ServerFailed = () => {
+  return (
+    <div className="server-failed center-col">
+      <PageTitle title="Server Failed" />
+      <h1>Server Failed &#9785;</h1>
+      <Link to="/" className="text-white">
+        Login Page
+      </Link>
+    </div>
+  );
+};
+
+export default ServerFailed;
