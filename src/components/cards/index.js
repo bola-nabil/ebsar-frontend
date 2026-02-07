cd7b304cad2c31fc9d4ca0ec8de77896634569a8
Bola Nabil <bolanabil404@gmail.com>
2026-02-07 15:46:05 +0200

    root of cards files

---
+++ src/components/cards/index.js
@@ -0,0 +1,5 @@
+export {default as BoxCard} from "./box-card/BoxCard";
+export {default as DetailsCard} from "./details-card/DetailsCard";
+export {default as HeaderCard} from "./header-card/HeaderCard";
+export {default as TitleCard} from "./title-card/TitleCard";
+
