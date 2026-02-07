d00e0f28dc5c73052b99cc7fbc1b7328eb8e3b00
Bola Nabil <bolanabil404@gmail.com>
2026-02-07 14:25:20 +0200

    root of categories

---
+++ src/features/categories/index.js
@@ -0,0 +1,3 @@
+export {default as CategoryCreate} from "./pages/CategoryCreate";
+export {default as CategoryEdit} from "./pages/CategoryEdit";
+export {default as Categories} from "./pages/Categories";
