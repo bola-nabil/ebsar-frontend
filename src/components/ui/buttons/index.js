0d20cf6a5efc375c37557f75ca50e0546b886feb
Bola Nabil <bolanabil404@gmail.com>
2026-02-07 15:45:40 +0200

    root of buttons files

---
+++ src/components/ui/buttons/index.js
@@ -0,0 +1,6 @@
+export {default as ControlsButtons} from "./control-button/ControlsButtons";
+export {default as DetailsButton} from "./details-button/DetailsButton";
+export {default as LoadingButton} from "./loading-button/LoadingButton";
+export {default as SubmitButton} from "./submit-button/SubmitButton";
+
+
