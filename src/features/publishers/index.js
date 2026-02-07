14e5dc88fd3533af6cb79032a1fff06ab5f4f638
Bola Nabil <bolanabil404@gmail.com>
2026-02-07 14:24:57 +0200

    root of publishers

---
+++ src/features/publishers/index.js
@@ -0,0 +1,4 @@
+export {default as PublisherCreate} from "./pages/PublisherCreate";
+export {default as PublisherEdit} from "./pages/PublisherEdit";
+export {default as Publishers} from "./pages/Publishers";
+
