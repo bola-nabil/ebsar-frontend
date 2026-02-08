f18fddd7164a982d127b1f1afc653a3bc4f6443b
Bola Nabil <bolanabil404@gmail.com>
2026-02-08 14:38:20 +0200

    add search card to include code and styles

---
+++ src/components/cards/search-card/SearchCard.jsx
@@ -0,0 +1,28 @@
+import {memo} from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import "./search-card.css";
+
+const SearchCard = ({searchTerm, setSearchTerm, handleSearch}) => {
+    const handleChange = (e) => setSearchTerm(e.target.value);
+  return (
+    <div className="card-search center-row text-white">
+        <input
+          type="text"
+          className="fw-bold rounded-4 w-75"
+          value={searchTerm}
+          placeholder="Search...."
+          onChange={handleChange}
+        />
+        <button onClick={handleSearch} 
+            className="search rounded-2 border-0" 
+            type='button' 
+            aria-label='Search'
+        >
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+    </div>
+  )
+}
+
+export default memo(SearchCard);
