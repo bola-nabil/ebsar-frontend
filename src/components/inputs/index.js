2bdfc06f1832046a0199fcddfe065907275f8b7f
Bola Nabil <bolanabil404@gmail.com>
2026-02-08 12:37:15 +0200

    root of inputs files

---
+++ src/components/inputs/index.js
@@ -0,0 +1,5 @@
+export {default as TextInput} from "./TextInput";
+export {default as FileInput} from "./FileInput";
+export {default as SelectInput} from "./SelectInput";
+export {default as MultiSelectInput} from "./MultiSelectInput";
+
