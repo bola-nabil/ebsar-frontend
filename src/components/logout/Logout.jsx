31b25695484f39797fd8fc324ce4488a2774e5be
Bola Nabil <bolanabil404@gmail.com>
2026-02-10 13:54:39 +0200

    improve code

--- src/components/logout/Logout.jsx
+++ src/components/logout/Logout.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import "./logout.css";
 
 const Logout = ({onLogout}) => {
   return (
-    <div className="logout-msg rounded-2 position-absolute">
     <div className="msg text-center">
         <button className="text-white rounded-2 fw-bold bg-primary p-2 rounded-2 border-0" onClick={onLogout }>
             Logout
         </button>
-    </div>
     </div>
   )
 }
