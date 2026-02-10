17034e2cd4654efb91e5285f7b45a387196caf58
Bola Nabil <bolanabil404@gmail.com>
2026-02-10 13:53:52 +0200

    add padding in the top

--- src/components/dashboard/dashboard-counts/DashboardCounts.jsx
+++ src/components/dashboard/dashboard-counts/DashboardCounts.jsx
@@ -4,23 +4,22 @@ import "./dashboard-counts.css";
 
 const DashboardCounts = ({dashboardCounts}) => {
   return (
-          <div className="dashboard-counts">
-            {dashboardCounts.map((card) => (
-              <div
-                className="dash-card bg-white rounded-4 center-row"
-                key={card.id}
-              >
-                <div className="card-icon text-white rounded-3">
-                  <FontAwesomeIcon icon={card.icon} />
-                </div>
-                <div className="card-info">
-                  <h3 className="fw-bold">{card.nums}</h3>
-                  <p>{card.title}</p>
-                </div>
+        <div className="dashboard-counts">
+          {dashboardCounts.map((card) => (
+            <div
+              className="dash-card bg-white rounded-4 center-row"
+              key={card.id}
+            >
+              <div className="card-icon text-white rounded-3">
+                <FontAwesomeIcon icon={card.icon} />
               </div>
-            ))}
-          </div>
-    
+              <div className="card-info">
+                <h3 className="fw-bold">{card.nums}</h3>
+                <p>{card.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
   )
 }
 
