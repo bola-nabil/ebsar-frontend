69c1886e0f05353b17a3d96e040dca91246c5436
Bola Nabil <bolanabil404@gmail.com>
2026-02-11 15:36:05 +0200

    improve code

--- src/components/cards/title-card/TitleCard.jsx
+++ src/components/cards/title-card/TitleCard.jsx
@@ -4,16 +4,20 @@ import "./title-card.css";
 
 const TitleCard = ({ title, pathCard, titleCard }) => {
   return(
-    <div className="title-card d-flex align-items-center justify-content-between">
-      <h2>{title}</h2>
-      <div className="card-btn">
-        <Link
-          to={pathCard}
-          aria-label={titleCard}
-          className="fw-medium text-decoration-none p-3 text-white rounded-3"
-        >
-          {titleCard}
-        </Link>
+    <div className="title-card-root position-fixed">
+      <div className="title-card-content">
+          <div className="title">
+              <h2>{title}</h2>
+          </div>
+          <div className="btn">
+            <Link
+              to={pathCard}
+              aria-label={titleCard}
+              className="fw-medium text-decoration-none p-3 text-white rounded-3"
+            >
+              {titleCard}
+            </Link>
+          </div>
       </div>
     </div>
   )
