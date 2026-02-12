15c0c83b17bcd59a1d6dd4862eb0958ad385daf1
Bola Nabil <bolanabil404@gmail.com>
2026-02-12 15:35:37 +0200

    improve code

--- src/components/ui/error/error-message/ErrorMessage.jsx
+++ src/components/ui/error/error-message/ErrorMessage.jsx
@@ -4,15 +4,13 @@ import "./error-message.css";
 const ErrorMessage = ({ message }) => {
   return (
     <>
-      {message && (
-        <div className="danger-msg center-row">
-          <div className="msg">
-            <p className="fw-bold fs-4 text-danger text-center text-uppercase">
-              {message}
-            </p>
-          </div>
+     {
+      message && (
+         <div className="error-message center-col">
+            <p className="fw-bold fs-4 text-danger text-center text-uppercase">{message}</p>
         </div>
-      )}
+      )
+     }
     </>
   );
 };
