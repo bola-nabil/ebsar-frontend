e8c97238c431dfcf019a76a30e3f080e381bde9c
Bola Nabil <bolanabil404@gmail.com>
2026-02-12 15:39:38 +0200

    edit path of import api

--- src/components/dashboard/DonutChart.jsx
+++ src/components/dashboard/DonutChart.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { PieChart, Pie, Cell, Legend, ResponsiveContainer } from "recharts";
-import { api } from "../../api";
+import { api } from "api";
 
 const COLORS = ["#6366f1", "#8b5cf6", "#facc15", "#f87171"];
 
