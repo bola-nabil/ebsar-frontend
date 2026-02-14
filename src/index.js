c01518f77096715c6b9e0a9c5b024d2c2e6d348d
Bola Nabil <bolanabil404@gmail.com>
2026-02-14 17:51:14 +0200

    add fonts.css file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/global.css";
-import "./styles/animations.css";
+import "styles/global.css";
+import "styles/animations.css";
+import "styles/fonts.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
