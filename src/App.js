98d73a0bbea4d7d4bbb96c6126f5713490552325
Bola Nabil <bolanabil404@gmail.com>
2026-02-15 15:07:24 +0200

    add layouts of routers pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AdminLogin from "./layouts/login/AdminLogin";
 import ChangePassword from "./pages/ChangePassword";
 import Dashboard from "pages/Dashboard";
@@ -8,44 +8,36 @@ import {CategoryCreate, CategoryEdit, Categories} from "features/categories";
 import {PublisherCreate, PublisherEdit, Publishers} from "features/publishers";
 import {BookCreate, BookEdit, Books} from "features/books";
 import Users from "./features/users/pages/Users";
-import NavBar from "./components/navBar/NavBar";
 import Policy from "./pages/Policy";
 import NotFound from "./pages/not-found/NotFound";
 import ServerFailed from "./pages/server-failed/ServerFailed";
+import MainLayout from "layouts/MainLayout";
 
 const App = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const location = useLocation();
-  const hideNavBarPaths = ["/", "/server-failed"];
-  const shouldHideNavBar = hideNavBarPaths.includes(location.pathname);
   return (
     <div className="app">
-      {!shouldHideNavBar && (
-        <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      )}
-      {!menuOpen && (
         <Routes>
           <Route path="/" element={<AdminLogin />} />
-          <Route path="/change-password" element={<ChangePassword />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/authors" element={<Authors />} />
-          <Route path="/authors/create" element={<AuthorCreate />} />
-          <Route path="/authors/:id/edit" element={<AuthorEdit />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/categories/create" element={<CategoryCreate />} />
-          <Route path="/categories/:id/edit" element={<CategoryEdit />} />
-          <Route path="/publishers/" element={<Publishers />} />
-          <Route path="/publishers/create" element={<PublisherCreate />} />
-          <Route path="/publishers/:id/edit" element={<PublisherEdit />} />
-          <Route path="/books" element={<Books />} />
-          <Route path="/books/create" element={<BookCreate />} />
-          <Route path="/books/:id/edit" element={<BookEdit />} />
-          <Route path="/users" element={<Users />} />
+          <Route path="/change-password" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><ChangePassword /></MainLayout>} />
+          <Route path="/dashboard" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><Dashboard /></MainLayout>} />
+          <Route path="/authors" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><Authors /></MainLayout>} />
+          <Route path="/authors/create" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><AuthorCreate /></MainLayout>} />
+          <Route path="/authors/:id/edit" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><AuthorEdit /></MainLayout>} />
+          <Route path="/categories" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><Categories /></MainLayout>} />
+          <Route path="/categories/create" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><CategoryCreate /></MainLayout>} />
+          <Route path="/categories/:id/edit" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><CategoryEdit /></MainLayout>} />
+          <Route path="/publishers/" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><Publishers /></MainLayout>} />
+          <Route path="/publishers/create" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><PublisherCreate /></MainLayout>} />
+          <Route path="/publishers/:id/edit" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><PublisherEdit /></MainLayout>} />
+          <Route path="/books" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><Books /></MainLayout>} />
+          <Route path="/books/create" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><BookCreate /></MainLayout>} />
+          <Route path="/books/:id/edit" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><BookEdit /></MainLayout>} />
+          <Route path="/users" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><Users /></MainLayout>} />
           <Route path="/policy" element={<Policy />} />
           <Route path="/server-failed" element={<ServerFailed />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<MainLayout menuOpen={menuOpen} setMenuOpen={setMenuOpen}><NotFound /></MainLayout>}/>
         </Routes>
-      )}
     </div>
   );
 };
